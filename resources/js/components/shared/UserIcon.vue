ff892b792321d2d10c5da42be3b1e951bf7c4148
Radan Stupar <radan.s@hotmail.com>
2024-02-05 10:26:39 +0100

    User status updated

--- resources/js/components/shared/UserIcon.vue
+++ resources/js/components/shared/UserIcon.vue
@@ -1,38 +1,60 @@
 <script setup lang="ts">
 import { FwbAvatar } from "flowbite-vue";
 import { useUserStore } from "../../store/user";
-import { AvatarSize } from "flowbite-vue/dist/components/FwbAvatar/types";
+import {
+    AvatarSize,
+    AvatarStatus,
+} from "flowbite-vue/dist/components/FwbAvatar/types";
+import { ChatResource, UserResource } from "@/types";
+import { toNumber } from "lodash";
 const userState = useUserStore();
 
 const props = withDefaults(
     defineProps<{
-        user: any;
+        user: ChatResource | UserResource;
         status: boolean;
         avatarSize: AvatarSize;
     }>(),
     {
-        user: null,
         status: true,
         avatarSize: "sm",
     }
 );
 
-const getUserStatus = () => {
-    return userState.onlineUsers.filter((obj) => obj.id === props.user?.id)
-        .length;
+const getUserStatus = (): AvatarStatus => {
+    console.log({
+        onlineUsers: userState.onlineUsers,
+        id: typeof props.user,
+        filter: userState.onlineUsers.filter(
+            (obj) => obj.id === props.user?.id
+        ),
+    });
+
+    let usersOnlineLength = userState.onlineUsers.filter(
+        (obj) => obj.id === (props.user as UserResource)?.id
+    ).length;
+
+    if ("chatable_id" in props.user) {
+        usersOnlineLength = userState.onlineUsers.filter(
+            (obj) =>
+                obj.id === toNumber((props.user as ChatResource)?.chatable_id)
+        ).length;
+    }
+
+    return usersOnlineLength ? "online" : "away";
 };
+
+console.log({ obj: props.user });
 </script>
 
 <template>
-    <span class="relative overflow-hidden flex gap-0.5 items-center">
-        <div
-            v-if="status"
-            :class="{
-                'w-2 h-2 rounded z-10': true,
-                'bg-green-600': getUserStatus(),
-            }"
+    <span class="relative flex gap-0.5 items-center">
+        <fwb-avatar
+            :size="avatarSize"
+            :img="user?.avatar"
+            status-position="top-right"
+            :status="getUserStatus()"
         />
-        <fwb-avatar :size="avatarSize" :img="user?.avatar" />
     </span>
 </template>
 
