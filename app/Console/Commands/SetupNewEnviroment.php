b0a018174df7fab13655c7d9c7e7d257ba6431a0
Radan Stupar <radan.s@hotmail.com>
2024-03-01 13:33:31 +0100

    Ready for prod

--- app/Console/Commands/SetupNewEnviroment.php
+++ app/Console/Commands/SetupNewEnviroment.php
@@ -33,26 +33,8 @@ class SetupNewEnviroment extends Command
         $this->info("Loading enviroment");
         $this->model = SetEnviroment::loadEnv($connection);
         $this->info("Loaded enviroment");
-        $this->info("Setting up database");
-        $this->prepareDB();
         SetEnviroment::connection($this->model);
         Artisan::call('migrate --seed');
     }
 
-    private function prepareDB()
-    {
-        $databaseName = $this->model->database_name;
-        $username = $this->model->database_name;
-        $password = $this->model->database_password;
-        $this->info("Creating db");
-        \DB::statement("CREATE DATABASE IF NOT EXISTS $databaseName");
-
-
-        $this->info("Creating user");
-        \DB::statement("CREATE USER '$username'@'%' IDENTIFIED BY '$password'");
-        $this->info("Adding privilages");
-        \DB::statement("GRANT ALL PRIVILEGES ON $databaseName.* TO '$username'@'%'");
-        $this->info("Added privilages");
-
-    }
 }
