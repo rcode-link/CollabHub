072575deef6986f3459237abfe04c0ff6def532c
Radan Stupar <radan.s@hotmail.com>
2024-04-11 18:17:54 +0200

    Service worker url

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,29 @@ export default defineConfig({
             workbox: {
                 globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
             },
+            includeAssets: [
+                "favicon.ico",
+                "apple-touch-icon.png",
+                "mask-icon.svg",
+            ],
+            manifest: {
+                name: "My Awesome App",
+                short_name: "MyApp",
+                description: "My Awesome App description",
+                theme_color: "#ffffff",
+                icons: [
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
             devOptions: {
                 enabled: true,
             },
