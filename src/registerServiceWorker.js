8b23580f4fc6bbf7b4d6388bca873e339f5fd20d
Radan Stupar <radan.s@hotmail.com>
2024-04-11 17:52:16 +0200

    Service worker url

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,7 +2,7 @@
 
 import { register } from "register-service-worker";
 
-register(`${process.env.BASE_URL}service-worker.js`, {
+register(`/service-worker.js`, {
     ready() {
         console.log(
             "App is being served from cache by a service worker.\n" +
