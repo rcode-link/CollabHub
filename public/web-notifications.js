9702243528fcf898882cb2a20e994f7813890921
Radan Stupar <radan.s@hotmail.com>
2025-04-09 21:22:13 +0200

    Web worker

--- public/web-notifications.js
+++ public/web-notifications.js
@@ -12,6 +12,8 @@ if ('serviceWorker' in navigator) {
     .then(function (serviceWorkerRegistration) {
       const sub = serviceWorkerRegistration.pushManager.subscribe({
         userVisibleOnly: true,
+        applicationServerKey:
+          'BFHChgedr72giDXoNxYixadXfFXg9UNSf2iJrI-S3s3TSjZGnuaNHKn6RiTrzt86NbVZTsaFrs4Lq3N11NXKgD8',
       })
       console.log(sub)
     })
