3f347a01f7fae8dd6af5ea8ea26a0971a339c8e9
Radan Stupar <radan.s@hotmail.com>
2025-04-09 20:50:38 +0200

    Web worker

--- public/web-notifications.js
+++ public/web-notifications.js
@@ -6,64 +6,12 @@ if ('serviceWorker' in navigator) {
         console.log('Service Worker registered with scope:', registration.scope)
         return registration.ready
       } else {
-        throw new Error('Service Worker registration returned undefined.')
+        console.error('Service Worker registration returned undefined.')
       }
-    })
-    .then(function (registration) {
-      if (registration.active && registration.active.pushManager) {
-        return registration.active.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: urlBase64ToUint8Array(
-            'BFHChgedr72giDXoNxYixadXfFXg9UNSf2iJrI-S3s3TSjZGnuaNHKn6RiTrzt86NbVZTsaFrs4Lq3N11NXKgD8',
-          ),
-        })
-      } else {
-        throw new Error(
-          'Push Manager is not available on the active service worker.',
-        )
-      }
-    })
-    .then(function (subscription) {
-      const token = localStorage.getItem('token')
-      fetch('/api/v1/push-notification', {
-        method: 'POST',
-        body: JSON.stringify(subscription),
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      }).then(response => {
-        if (!response.ok) {
-          console.error('Push subscription failed:', response.statusText)
-        } else {
-          console.log('Push subscription successful.')
-        }
-      })
     })
     .catch(function (error) {
-      console.error('Service Worker or push subscription error:', error)
+      console.error('Service Worker registration failed:', error)
     })
 } else {
-  console.error('Service workers are not supported in this browser.')
-}
-
-Notification.requestPermission().then(function (result) {
-  if (result === 'granted') {
-    console.log('Notification permission granted.')
-  } else {
-    console.error('Notification permission denied.')
-  }
-})
-
-function urlBase64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
-  const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
-
-  const rawData = window.atob(base64)
-  const outputArray = new Uint8Array(rawData.length)
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i)
-  }
-  return outputArray
+  console.warn('Service Workers are not supported in this browser.')
 }
