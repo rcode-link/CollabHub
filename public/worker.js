0c4e0c170935fd86c3075d92f4e7d6a860febebb
Radan Stupar <radan.s@hotmail.com>
2025-04-09 20:51:53 +0200

    Web worker

--- public/worker.js
+++ public/worker.js
@@ -1,47 +1,57 @@
 self.addEventListener('push', event => {
-    console.log('Push event received:', event)
-    const data = event.data.json()
+  console.log('Push event received:', event)
+  const data = event.data.json()
 
-    console.log(data)
-    // Show notification with the icon URL from the notification data
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: data.icon, // Use the icon URL from the notification data
-        data: {
-            chat_id: data.data.chat_id, // Ensure chat_id is included in the notification data
-        },
-        actions: [{ action: 'notification_action', title: 'View App' }],
-    })
+  console.log(data)
+  // Show notification with the icon URL from the notification data
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: data.icon, // Use the icon URL from the notification data
+    data: {
+      chat_id: data.data.chat_id, // Ensure chat_id is included in the notification data
+    },
+    actions: [{ action: 'notification_action', title: 'View App' }],
+  })
+})
+
+self.addEventListener('install', event => {
+  console.log('Service Worker installing.')
+  // Perform installation steps
+})
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activating.')
+  // Perform activation steps
 })
 
 self.addEventListener('notificationclick', event => {
-    console.log('Notification click received:', event)
+  console.log('Notification click received:', event)
 
-    // Close the notification
-    event.notification.close()
+  // Close the notification
+  event.notification.close()
 
-    // Get the chat_id from the notification data
-    const chatId = event.notification.data.chat_id
-    if (!chatId) {
-        console.error('Chat ID is undefined')
-        return
-    }
+  // Get the chat_id from the notification data
+  const chatId = event.notification.data.chat_id
+  if (!chatId) {
+    console.error('Chat ID is undefined')
+    return
+  }
 
-    const notificationUrl = `/chat/${chatId}/view` // Construct the URL using the chat_id
+  const notificationUrl = `/chat/${chatId}/view` // Construct the URL using the chat_id
 
-    event.waitUntil(
-        clients.matchAll({ type: 'window' }).then(windowClients => {
-            // Check if there is already a window/tab open with the target URL
-            for (let client of windowClients) {
-                // If so, focus it
-                if (client.url.endsWith(notificationUrl)) {
-                    return client.focus()
-                }
-            }
-            // If not, open a new window/tab with the target URL
-            if (clients.openWindow) {
-                return clients.openWindow(notificationUrl)
-            }
-        }),
-    )
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(windowClients => {
+      // Check if there is already a window/tab open with the target URL
+      for (let client of windowClients) {
+        // If so, focus it
+        if (client.url.endsWith(notificationUrl)) {
+          return client.focus()
+        }
+      }
+      // If not, open a new window/tab with the target URL
+      if (clients.openWindow) {
+        return clients.openWindow(notificationUrl)
+      }
+    }),
+  )
 })
