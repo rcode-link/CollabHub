1545a983ecc6f988d344099263aac721227b92ea
Radan Stupar <radan.s@hotmail.com>
2025-04-13 16:06:22 +0200

    Update: input ref

--- resources/js/components/shared/Text.vue
+++ resources/js/components/shared/Text.vue
@@ -11,7 +11,7 @@ defineProps({
 })
 defineEmits(["update:modelValue"]);
 
-const input = ref<HTMLInputElement | null>(null);
+const input = ref < HTMLInputElement | null > (null);
 
 onMounted(() => {
     if (input.value?.hasAttribute("autofocus")) {
@@ -23,18 +23,18 @@ defineExpose({ focus: () => input.value?.focus() });
 </script>
 
 <template>
-    <input
-        :class="{
-            'w-full': true,
-            'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500':
-                !errors.errors.hasOwnProperty(name),
-            'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500':
-                errors.errors.hasOwnProperty(name),
-        }"
-        :value="modelValue"
-        :id="name"
-        :disabled="disabled"
-        @input="$emit('update:modelValue', $event?.target?.value)"
-        ref="input"
-    />
+  <input
+    :class="{
+      'w-full': true,
+      'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500':
+        !errors.errors.hasOwnProperty(name),
+      'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500':
+        errors.errors.hasOwnProperty(name),
+    }"
+    :value="modelValue"
+    :id="name"
+    :disabled="disabled"
+    @input="$emit('update:modelValue', $event?.target?.value)"
+    :ref="input"
+  />
 </template>
