e38466b80453f9fc8a19e55174624adef3c226c0
Radan Stupar <radan.s@hotmail.com>
2025-05-06 10:18:19 +0200

    Commented out auto registration for notificaions

--- public/web-notifications.js
+++ public/web-notifications.js
@@ -9,26 +9,26 @@ if ('serviceWorker' in navigator) {
     })
 }
 
-navigator.serviceWorker.ready
-  .then(function (registration) {
-    return registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(
-        'BFHChgedr72giDXoNxYixadXfFXg9UNSf2iJrI-S3s3TSjZGnuaNHKn6RiTrzt86NbVZTsaFrs4Lq3N11NXKgD8',
-      ),
-    })
-  })
-  .then(function (subscription) {
-    const token = localStorage.getItem('token')
-    fetch('/api/v1/push-notification', {
-      method: 'POST',
-      body: JSON.stringify(subscription),
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    })
-  })
+// navigator.serviceWorker.ready
+//   .then(function (registration) {
+//     return registration.pushManager.subscribe({
+//       userVisibleOnly: true,
+//       applicationServerKey: urlBase64ToUint8Array(
+//         'BFHChgedr72giDXoNxYixadXfFXg9UNSf2iJrI-S3s3TSjZGnuaNHKn6RiTrzt86NbVZTsaFrs4Lq3N11NXKgD8',
+//       ),
+//     })
+//   })
+//   .then(function (subscription) {
+//     const token = localStorage.getItem('token')
+//     fetch('/api/v1/push-notification', {
+//       method: 'POST',
+//       body: JSON.stringify(subscription),
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//         'Content-Type': 'application/json',
+//       },
+//     })
+//   })
 
 function urlBase64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
