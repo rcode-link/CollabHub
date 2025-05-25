10f21bbd64cfbac87a5a2105be5814c359669da8
Radan Stupar <radan.s@hotmail.com>
2025-05-25 20:29:35 +0200

    Update: set env for profile image

--- app/Console/Commands/UpdateUsersProfile.php
+++ app/Console/Commands/UpdateUsersProfile.php
@@ -6,6 +6,8 @@ use App\Jobs\GenerateProfileImageJob;
 use App\Models\User;
 use Illuminate\Console\Command;
 
+use App\Helpers\SetEnviroment;
+
 class UpdateUsersProfile extends Command
 {
     /**
@@ -13,8 +15,9 @@ class UpdateUsersProfile extends Command
      *
      * @var string
      */
-    protected $signature = 'app:update-users-profile';
+    protected $signature = 'app:update-users-profile {env?}';
 
+    private $model;
     /**
      * The console command description.
      *
@@ -27,6 +30,14 @@ class UpdateUsersProfile extends Command
      */
     public function handle()
     {
+        $connection = $this->argument('env');
+        if ($connection) {
+            $this->info("Loading enviroment");
+            $this->model = SetEnviroment::loadEnv($connection);
+            $this->info("Loaded enviroment");
+            SetEnviroment::connection($this->model);
+        }
+
         User::doesnthave('media')->get()->map(fn(User $user) => GenerateProfileImageJob::dispatch($user));
         //
     }
